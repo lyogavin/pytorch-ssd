7746be20c0a778e1402c45bfbf302b504a9b15bc
Yu Li <lyo.gavin@gmail.com>
2020-04-30 12:04:17 -0700

    transform

--- eval_ssd_gen_prediction.py
+++ eval_ssd_gen_prediction.py
@@ -185,8 +185,8 @@ if __name__ == '__main__':
         parser.print_help(sys.stderr)
         sys.exit(1)
 
-    dataset.transform = predictor.transform
-    predictor.transform = None
+    dataset.transform = None #predictor.transform
+    #predictor.transform = None
 
 
     results = []
